b071bde0e9c5a850be16e4f19b7e291d84df4f66
Asaf <asaf757@gmail.com>
2021-08-26 13:11:50 +0100

    Service ready

--- Lib/better_APP.py
+++ Lib/better_APP.py
@@ -5,8 +5,9 @@ import better_config
 
 app = Flask(__name__)
 
-class bets_update():
-    def __init__(self,json,key):
+
+class bets_update:
+    def __init__(self, key):
         self.id = None
         self.type = None
         self.home = None
@@ -21,6 +22,7 @@ class bets_update():
         if "winner" in key:
             self.winner = key['match']['winner']
 
+
 @app.route('/form_register', methods=['POST'])
 def form_register():
     newusermail = request.args['email']
@@ -32,14 +34,15 @@ def form_register():
         return "Error. Username already exists"
     else:
         pass
-    query_put = """INSERT INTO dbo.users(u_mail, u_name, u_pass) VALUES ('{}','{}','{}');""".format(newusermail,newusername,newpassword)
+    query_put = """INSERT INTO dbo.users(u_mail, u_name, u_pass) VALUES ('{}','{}','{}');""".format(newusermail, newusername, newpassword)
     better_config.db_put(query_put)
     query_userid = """SELECT UID from dbo.users WHERE u_mail='{}'""".format(newusermail)
     user_id = better_config.db_pull_val(query_userid)
     print(user_id)
     return 'userID = {}'.format(user_id)
 
-@app.route('/form-login',methods=['POST'])
+
+@app.route('/form-login', methods=['POST'])
 def form_login():
     if request.method == 'POST':
         mail = request.args['email']
@@ -51,23 +54,25 @@ def form_login():
             user_id = better_config.db_pull_val(query_userid)
             return 'userID = {}'.format(user_id)
         else:
-            return ('Wrong username / password')
+            return 'Wrong username / password'
+
 
 @app.route('/pull-bets', methods=['GET'])
 def pullbets():
-    UID = request.args['UID']
-    LID = request.args['LID']
+    uid = request.args['UID']
+    lid = request.args['LID']
     query = """SELECT UID,n_type, m_status, m_hteam, m_ateam, m_hscore, m_ascore, m_outcome, m_winner
             FROM dbo.betlog
-            WHERE UID='{}', LID='{}'""".format(UID, LID)
+            WHERE UID='{}', LID='{}'""".format(uid, lid)
     response = better_config.db_pull_list(query)
     return response
 
-@app.route('/submit-bet',methods=['POST'])
+
+@app.route('/submit-bet', methods=['POST'])
 def submitbets():
     bets_json = request.get_json()
-    UID = bets_json['parameters']['UID']
-    LID = bets_json['parameters']['LID']
+    uid = bets_json['parameters']['UID']
+    lid = bets_json['parameters']['LID']
     if "bets" in bets_json:
         u_winner = bets_json['bets']['winner']
         u_goaler = bets_json['bets']['goaler']
@@ -77,28 +82,32 @@ def submitbets():
                               WHERE (UID='{}' AND LID='{}')
                           ELSE
                               INSERT INTO betlog (UID,LID,m_type,u_winner,u_goaler)
-                              VALUES ('{}','{}','winners','{}','{}')""".format(UID,LID,u_winner,u_goaler,UID,LID,UID,LID,u_winner,u_goaler)
+                              VALUES ('{}','{}','winners','{}','{}')""".format(uid, lid, u_winner, u_goaler, uid, lid, uid, lid, u_winner, u_goaler)
         better_config.db_put(query_bets)
     if "matches" in bets_json:
         for match in bets_json['matches']:
-            params = bets_update(bets_json,match)
+            params = bets_update(match)
             query_matches = """IF EXISTS (SELECT * FROM dbo.betlog WHERE (UID='{}' AND LID='{}' AND MID='{}'))
                                           UPDATE betlog
                                           SET m_type='{}',log_time='{}',b_hscore='{}',b_ascore='{}',b_outcome='{}',b_winner='{}'
                                           WHERE (UID='{}' AND LID='{}' AND MID='{}')
                                       ELSE
                                           INSERT INTO betlog (UID,LID,MID,m_type,log_time,b_hscore,b_ascore,b_outcome,b_winner)
-                                          VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(UID,LID,params.id,params.type,datetime.datetime.now(),
-                                                                                                          params.home,params.away,params.outcome,
-                                                                                                          params.winner,UID,LID,params.id,UID,LID,params.id,
-                                                                                                          params.type,datetime.datetime.now(),params.home,
-                                                                                                          params.away,params.outcome,params.winner)
+                                          VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(uid, lid, params.id, params.type,
+                                                                                                          datetime.datetime.now(), params.home,
+                                                                                                          params.away, params.outcome, params.winner,
+                                                                                                          uid, lid, params.id, uid, lid, params.id,
+                                                                                                          params.type, datetime.datetime.now(),
+                                                                                                          params.home, params.away, params.outcome,
+                                                                                                          params.winner)
             better_config.db_put(query_matches)
-    return ("Success")
+    return "Success"
+
+
 if __name__ == '__main__':
     # app.run(debug=True, port=5000)
     dbSyncer.sync_matches()
-    #db_syncer.sync_mastches()
+    # db_syncer.sync_mastches()
     # while True:
     #     # if x time passed since last timestamp, else pass
     #     this_run = datetime.datetime.now()
